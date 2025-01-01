9bbfc23f705375e365b86e2e96120a1d32d81539
Ismoil <ismoil@inverso.ai>
2025-01-01 23:18:51 +0300

    Added new domains && selectors

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -13,47 +13,71 @@ function removeParent(element, levels) {
 
 // Function for processing nodes
 function processNode(node) {
+    try {
+        const suggestCounter = node.getAttribute("data-suggest-counter");
+        if (suggestCounter && suggestCounter.startsWith("//yandex.ru/clck/safeclick")) {
+            node.remove();
+            return;
+        }
 
-    const suggestCounter = node.getAttribute("data-suggest-counter");
-    if (suggestCounter && suggestCounter.startsWith("//yandex.ru/clck/safeclick")) {
+        if (node.classList.contains("MMSidebar-Card_bottomAdv")) {
+            node.remove();
+            return;
+        }
+
+        const id = node.getAttribute("id");
+        if (id === "search-result-aside" || id === "click_area") {
+            node.remove();
+            return;
+        }
+
+        if (node.classList.contains("gcydtGWxoi")) {
+            removeParent(node, 3);
+            return;
+        }
+
+        const fullscreenElementName = node.getAttribute('data-fullscreen-element-name');
+        if (fullscreenElementName === 'close-btn') {
+
+            document.body.style.overflow = '';
+            removeParent(node, 4);
+            return;
+        }
+
+        if (node.tagName === 'IFRAME' && node.src && node.src.startsWith('https://yastatic.net/safeframe-bundles/')) {
+            removeParent(node, 7);
+        }
+        
+        if (node.id.startsWith('yandex_ad')) {
+            removeParent(node, 1);
+        }
+
+        if (node.classList.contains('DirectInlineContainer')) {
+            removeParent(node, 1);
+        }
+
         node.remove();
-        return;
-    }
-
-    if (node.classList.contains("MMSidebar-Card_bottomAdv")) {
-        node.remove();
-        return;
-    }
-
-    const id = node.getAttribute("id");
-    if (id === "search-result-aside" || id === "click_area") {
-        node.remove();
-        return;
-    }
-
-    if (node.classList.contains("gcydtGWxoi")) {
-        removeParent(node, 3);
-        return;
-    }
-
-    const fullscreenElementName = node.getAttribute('data-fullscreen-element-name');
-    if (fullscreenElementName === 'close-btn') {
-
-        document.body.style.overflow = '';
-        removeParent(node, 4);
-        return;
-    }
-
-    if (node.tagName === 'IFRAME' && node.src && node.src.startsWith('https://yastatic.net/safeframe-bundles/')) {
-        removeParent(node, 7);
-    }
+        
+    } catch (error) {}
 }
 
 // Function to process all nodes matching the criteria
 function processMatchingNodes() {
     // Finding all nodes that meet the criteria
     const nodesToProcess = [
-        ...document.querySelectorAll('[data-suggest-counter^="//yandex.ru/clck/safeclick"], .MMSidebar-Card_bottomAdv, #search-result-aside, #click_area, .gcydtGWxoi, [data-fullscreen-element-name="close-btn"], iframe[src^="https://yastatic.net/safeframe-bundles/"]')
+        ...document.querySelectorAll(
+            '[data-suggest-counter^="//yandex.ru/clck/safeclick"], ' +
+            '.MMSidebar-Card_bottomAdv, ' +
+            '#click_area, ' +
+            '.gcydtGWxoi, ' +
+            '[data-fullscreen-element-name="close-btn"], ' +
+            'iframe[src^="https://yastatic.net/safeframe-bundles/"], ' +
+            '#search-result li:has(.organic__path ~ span), ' +
+            '[data-testid="ad-banner"], ' + 
+            '[aria-label="Карточка рекламы"], ' +
+            '[id^="yandex_ad"], ' +
+            '.DirectInlineContainer'
+        )
     ];
 
     // Processing the found nodes
